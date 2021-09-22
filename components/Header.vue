2e4c1a129f054f3481d1f4c64ed5465d42f8dc61
gorkembaris.yesiltas <gorkembaris.yesiltas@student.kuleuven.be>
2021-09-22 19:16:20 +0200

    added social icons

--- components/Header.vue
+++ components/Header.vue
@@ -3,26 +3,42 @@
         <img src="../assets/gala-logo.png" class="header__gala-logo"/>
         <div class="header__menu-sections">
             <div class="header__menu-section">
-                <a href="#" class="header__menu-item">gs tv</a>
+                <img src="../assets/gstv.png" class="header__menu-icon w-10" />
                 <div class="header__seperator-line"></div>
                 <div class="header__menu-section">
-                    <FontAwesomeIcon :icon="['fab','facebook']" class="header__menu-item"/>
-                    <a href="#" class="header__menu-item">twitter</a>
-                    <a href="#" class="header__menu-item">instagram</a>
-                    <a href="#" class="header__menu-item">youtube</a>
+                    <FontAwesomeIcon 
+                        :icon="['fab','facebook']" 
+                        class="header__menu-icon" 
+                        size="lg"
+                    />
+                    <FontAwesomeIcon 
+                        :icon="['fab','twitter']" 
+                        class="header__menu-icon" 
+                        size="lg"
+                    />
+                    <FontAwesomeIcon 
+                        :icon="['fab','instagram']" 
+                        class="header__menu-icon" 
+                        size="lg"
+                    />
+                    <FontAwesomeIcon 
+                        :icon="['fab','youtube']" 
+                        class="header__menu-icon" 
+                        size="lg"
+                    />
                 </div>
                 <div class="header__seperator-line"></div>
-                <a href="#" class="header__menu-item">TR</a>
+                <a href="#" class="header__menu-icon">TR</a>
             </div>
             <div class="header__menu-section">
-                <a href="#" class="header__menu-item">GALATASARAY SK</a>
-                <a href="#" class="header__menu-item">FUTBOL</a>
-                <a href="#" class="header__menu-item">BASKETBOL</a>
-                <a href="#" class="header__menu-item">VOLEYBOL</a>
-                <a href="#" class="header__menu-item">BRANSLAR</a>
-                <a href="#" class="header__menu-item">TESISLER</a>
-                <a href="#" class="header__menu-item">GSSTORE</a>
-                <a href="#" class="header__menu-item">TARIHCE</a>
+                <a href="#" class="header__menu-title">GALATASARAY SK</a>
+                <a href="#" class="header__menu-title">FUTBOL</a>
+                <a href="#" class="header__menu-title">BASKETBOL</a>
+                <a href="#" class="header__menu-title">VOLEYBOL</a>
+                <a href="#" class="header__menu-title">BRANSLAR</a>
+                <a href="#" class="header__menu-title">TESISLER</a>
+                <a href="#" class="header__menu-title">GSSTORE</a>
+                <a href="#" class="header__menu-title">TARIHCE</a>
             </div>
         </div>
     </div>
@@ -40,8 +56,11 @@
     &__menu-section {
         @apply flex space-x-4 relative h-full items-center;
     }
-    &__menu-item {
-        @apply text-yellow-500 text-xs hover:bg-gray-900 px-2 py-4;
+    &__menu-icon {
+        @apply text-yellow-500 cursor-pointer;
+    }
+    &__menu-title {
+        @apply text-yellow-500 text-xs cursor-pointer;
     }
     &__seperator-line {
         @apply w-px h-8 bg-yellow-500;
