edfc4a08bfab92c738705eed9912edd545a394ed
gorkembaris.yesiltas <gorkembaris.yesiltas@student.kuleuven.be>
2022-01-30 10:07:31 +0100

    social links now open in new link

--- components/molecules/HeaderSocials.vue
+++ components/molecules/HeaderSocials.vue
@@ -1,27 +1,27 @@
 <template>
     <div>
-        <a href="https://www.facebook.com/Galatasaray/">
+        <a href="https://www.facebook.com/Galatasaray/" rel="noopener noreferrer" target="_blank">
             <FontAwesomeIcon 
                 :icon="['fab','facebook']" 
                 class="menu-icon" 
                 size="lg"
             />
         </a>
-        <a href="https://twitter.com/GalatasaraySK">
+        <a href="https://twitter.com/GalatasaraySK" rel="noopener noreferrer" target="_blank">
             <FontAwesomeIcon 
                 :icon="['fab','twitter']" 
                 class="menu-icon" 
                 size="lg"
         />
         </a>
-        <a href="https://www.instagram.com/galatasaray/">
+        <a href="https://www.instagram.com/galatasaray/" rel="noopener noreferrer" target="_blank">
             <FontAwesomeIcon 
                 :icon="['fab','instagram']" 
                 class="menu-icon" 
                 size="lg"
         />
         </a>
-        <a href="https://www.youtube.com/user/2galatasaray">
+        <a href="https://www.youtube.com/user/2galatasaray" rel="noopener noreferrer" target="_blank">
             <FontAwesomeIcon 
                 :icon="['fab','youtube']" 
                 class="menu-icon" 
